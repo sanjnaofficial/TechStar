45923b660b37dda1310dd17e5b23e5b98dcbe9f8
sanjnakritter <sanjna.sanjna@kritter.in>
2023-06-28 11:21:59 +0530

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {HashLink} from 'react-router-hash-link'
+
+const Header = () => {
+  return (
+    <div>
+        <nav>
+            <h1>TechStar</h1>
+            <main>
+                <HashLink to={"/#home"}>Home</HashLink>
+                <Link to={"/contact"}>Contact</Link>
+                <HashLink to={"/#about"}>About</HashLink>
+                <HashLink to={"/#brand"}>Brands</HashLink>
+                <Link to={"/services"}>Services</Link>
+            </main>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
